1481ae1f85098da21f4cbc01df6b29bc0ba8306a
Fabio Masciarelli <fabiomasciarelliwd@gmail.com>
2024-04-22 16:43:23 +0200

    add: click for select chat

--- js/script.js
+++ js/script.js
@@ -3,53 +3,16 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            contacts: [
-                {
-                    name:"Waltere",
-                    profilePhoto:"./img/avatar_1.jpg",
-                    message: "Apri un ticket, oppure studia per battere Loris a scacchi"
-                },
-                {
-                    name:"Luca Giardina",
-                    profilePhoto:"./img/avatar_2.jpg",
-                    message: "Apri un ticket, oppure studia per battere Loris a scacchi"
-                },
-                {
-                    name:"Simone Bonvecchio",
-                    profilePhoto:"./img/avatar_3.jpg",
-                    message: "Apri un ticket, oppure studia per battere Loris a scacchi"
-                },
-                {
-                    name:"Berlusconi",
-                    profilePhoto:"./img/avatar_4.jpg",
-                    message: "Apri un ticket, oppure studia per battere Loris a scacchi"
-                },
-                {
-                    name:"Loris Barbiero",
-                    profilePhoto:"./img/avatar_5.jpg",
-                    message: "Apri un ticket, oppure studia per battere Loris a scacchi"
-                },
-                {
-                    name:"Olga Demina",
-                    profilePhoto:"./img/avatar_6.jpg",
-                    message: "Apri un ticket, oppure studia per battere Loris a scacchi"
-                },
-                {
-                    name:"Vittorio Sgarbi",
-                    profilePhoto:"./img/avatar_7.jpg",
-                    message: "Apri un ticket, oppure studia per battere Loris a scacchi"
-                },
-                {
-                    name:"Balotelli",
-                    profilePhoto:"./img/avatar_8.jpg",
-                    message: "Apri un ticket, oppure studia per battere Loris a scacchi"
-                }
-            ]
+            contacts,
+            curChat: 0
         }
     },
     methods: {
         selectedChat(index) {
-            
+            console.log(index);
+            this.curChat = index;
         }
     }
 }).mount(".viewport")
+
+
