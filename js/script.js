6810646f94c3a26c053e7e430368eeb96d97b86f
Fabio Masciarelli <fabiomasciarelliwd@gmail.com>
2024-04-22 17:48:06 +0200

    add: current chat at click

--- js/script.js
+++ js/script.js
@@ -4,13 +4,18 @@ createApp({
     data() {
         return {
             contacts,
-            curChat: 0
+            curChat: 0,
+            sent:"",
         }
     },
     methods: {
         selectedChat(index) {
             console.log(index);
             this.curChat = index;
+        },
+        
+        send(index) {
+            
         }
     }
 }).mount(".viewport")
